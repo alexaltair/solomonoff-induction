85dcb3f1f777261220676d5c68c79bd4e3a61d80
Alex Altair <alexanderaltair@gmail.com>
2014-11-15 22:32:06 -0800

    Add neat example TM.

--- turing_machine/examples.py
+++ turing_machine/examples.py
@@ -12,3 +12,23 @@ wolfram_smith = tm.TuringMachine(2, [[False,  True, 2, True,  False, 1, False,  
                                      random.randint(0, 2**16))
 # wolfram_smith.tape = [None]*16 + wolfram_smith.tape
 # wolfram_smith.head = 20
+
+
+
+
+neat = tm.TuringMachine(12,
+  [
+    [False, False,  4, False, True,   2, True,  False,  9],
+    [True,  False, 12, False, False,  0, True,  True,   7],
+    [True,  False,  6, False, False, 12, False, False,  1],
+    [True,  True,   7, True,  False, 11, False, False,  6],
+    [False, True,  11, False, False,  5, False, True,   5],
+    [False, True,   4, False, False, 12, True,  True,  10],
+    [False, True,   8, True,  True,   3, True,  True,  11],
+    [False, False,  7, True,  False, 12, True,  False, 10],
+    [True,  False,  9, True,  True,   5, False, False, 11],
+    [False, False,  3, True,  True,   2, True,  False,  6],
+    [True,  False, 10, True,  False, 11, False, True,  11],
+    [True,  False,  3, True,  False,  8, True,  False,  2]
+  ],
+  random.randint(0, 2**32))
